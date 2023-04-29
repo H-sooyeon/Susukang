1c7f758e8dc024c9f88658d9abe715f3cc9856fc
H-sooyeon <wndqhr__@naver.com>
2023-04-30 00:22:59 +0900

    add login page navigation

--- App.js
+++ App.js
@@ -4,35 +4,44 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MinutesScreen from './screens/MinutesScreen';
 import ChattingScreen from './screens/ChattingScreen';
+import SignInScreen from './screens/SignInScreen';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App() {
   const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Minutes">
-        <Stack.Screen
-          name="SettingOption"
-          component={TranslationSettingsScreen}
-          options={{
-            title: '옵션 선택',
-          }}
-          style={{flex: 1}}
-        />
-        <Stack.Screen
-          name="Minutes"
-          component={MinutesScreen}
-          options={{
-            title: '회의록',
-          }}
-        />
-        <Stack.Screen
-          name="Chatting"
-          component={ChattingScreen}
-          options={{title: '번역'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SignIn">
+          <Stack.Screen
+            name="SignIn"
+            component={SignInScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="SettingOption"
+            component={TranslationSettingsScreen}
+            options={{
+              title: '옵션',
+            }}
+            style={{flex: 1}}
+          />
+          <Stack.Screen
+            name="Minutes"
+            component={MinutesScreen}
+            options={{
+              title: '회의록',
+            }}
+          />
+          <Stack.Screen
+            name="Chatting"
+            component={ChattingScreen}
+            options={{title: '번역'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
