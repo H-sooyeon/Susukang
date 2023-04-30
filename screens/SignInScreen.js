448b358b9a1ff51eeb73b5f7688c9c53a6b1177a
H-sooyeon <wndqhr__@naver.com>
2023-04-30 13:41:53 +0900

    distribute component

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,53 +1,70 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {
+  Alert,
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {View, StyleSheet, Text, StatusBar} from 'react-native';
-import BorderedInput from '../components/BorderedInput';
-import CustomButton from '../components/CustomButton';
+import SignButtons from '../components/SignButtons';
+import SignInForm from '../components/SignForm';
+import {signIn, signUp} from '../lib/auth';
 
 const SignInScreen = ({navigation, route}) => {
   const {isSignUp} = route.params ?? {};
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [loading, setLoading] = useState();
+
+  const createChangeTextHandler = name => value => {
+    setForm({...form, [name]: value});
+  };
+
+  const onSubmit = async () => {
+    //Keyboard.dismiss();
+    const {email, password} = form;
+    const info = {email, password};
+    setLoading(true);
+    try {
+      const {user} = isSignUp ? await signUp(info) : await signIn(info);
+      console.log(user);
+    } catch (e) {
+      Alert.alert('실패');
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <SafeAreaView style={styles.fullscreen}>
-      <StatusBar backgroundColor="white" barStyle="dark-content" />
-      <Text style={styles.text}>Susukang</Text>
-      <View style={styles.form}>
-        <BorderedInput hasMarginBottom placeholder="이메일" />
-        <BorderedInput placeholder="비밀번호" hasMarginBottom={isSignUp} />
-        {isSignUp && <BorderedInput placeholder="비밀번호 확인" />}
-        <View style={styles.buttons}>
-          {isSignUp ? (
-            <>
-              <CustomButton title="회원가입" hasMarginBottom />
-              <CustomButton
-                title="로그인"
-                theme="secondary"
-                onPress={() => {
-                  navigation.goBack();
-                }}
-              />
-            </>
-          ) : (
-            <>
-              <CustomButton
-                title="로그인"
-                hasMarginBottom
-                onPress={() => {
-                  navigation.navigate('Minutes');
-                }}
-              />
-              <CustomButton
-                title="회원가입"
-                theme="secondary"
-                onPress={() => {
-                  navigation.push('SignIn', {isSignUp: true});
-                }}
-              />
-            </>
-          )}
+    <KeyboardAvoidingView
+      style={styles.keyboardAvoidingView}
+      behavior={Platform.select({ios: 'padding'})}>
+      <SafeAreaView style={styles.fullscreen}>
+        <StatusBar backgroundColor="white" barStyle="dark-content" />
+        <Text style={styles.text}>Susukang</Text>
+        <View style={styles.form}>
+          <SignInForm
+            isSignUp={isSignUp}
+            onSubmit={onSubmit}
+            form={form}
+            createChangeTextHandler={createChangeTextHandler}
+          />
+          <SignButtons
+            isSignUp={isSignUp}
+            onSubmit={onSubmit}
+            loading={loading}
+          />
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -67,8 +84,8 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingHorizontal: 16,
   },
-  buttons: {
-    marginTop: 64,
+  keyboardAvoidingView: {
+    flex: 1,
   },
 });
 
