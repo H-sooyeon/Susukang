2043b7e6b76066cc852b46f881c4bf9fc93d2656
H-sooyeon <wndqhr__@naver.com>
2023-05-05 02:53:49 +0900

    feat: 제목 입력하여 문서 추가

---
+++ components/AddMinute.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const AddMinue = ({onInsert}) => {
+  const [title, setTitle] = useState('');
+
+  const onPress = () => {
+    onInsert(title);
+    setTitle('');
+    Keyboard.dismiss();
+  };
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="제목을 입력하세요."
+        style={styles.input}
+        value={title}
+        onChangeText={setTitle}
+        onSubmitEditing={onPress}
+        returnKeyType="done"
+      />
+      <TouchableOpacity style={styles.Addbutton} onPress={onPress}>
+        <Icon name="add-circle" size={30} color="#1976D2" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {
+    height: 55,
+    paddingHorizontal: 16,
+    borderColor: '#bdbdbd',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    fontSize: 17,
+    paddingVertical: 8,
+  },
+});
+
+export default AddMinue;
