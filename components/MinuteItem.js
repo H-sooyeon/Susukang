ca4e222eef399603ad65063c035b308ec236646e
H-sooyeon <wndqhr__@naver.com>
2023-05-06 23:53:59 +0900

    feat: 수정 버튼 클릭 시 Dialog 출력

--- components/MinuteItem.js
+++ components/MinuteItem.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useNavigation} from '@react-navigation/native';
+import Dialog from 'react-native-dialog';
 
 const MinuteItem = ({
   id,
@@ -11,14 +12,43 @@ const MinuteItem = ({
   content,
   onRemove,
   datas,
+  onToggle,
+  getDate,
 }) => {
   const navigation = useNavigation();
 
-  const onPress = () => {};
+  const [visible, setVisible] = useState(false);
+  const [itemTitle, setitemTitle] = useState(title);
+  const [itemDepartment, setitemDepartment] = useState(department);
+
+  const showDialog = () => {
+    setVisible(true);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const handleOk = () => {
+    const today = new Date();
+
+    if (itemTitle === '') {
+      Alert.alert('제목은 공백으로 둘 수 없습니다.');
+
+      setitemTitle(title);
+      setitemDepartment(department);
+    } else {
+      onToggle(id, itemTitle, itemDepartment, getDate(today));
+      // 서버에 저장된 문서 정보 수정하는 코드 추가
+    }
+    setVisible(false);
+  };
+
+  const DownFile = () => {};
 
   return (
     <View style={styles.item}>
-      <TouchableOpacity style={styles.Icon}>
+      <TouchableOpacity style={styles.Icon} onPress={DownFile}>
         <Icon name="arrow-circle-down" size={30} color="#1976D2" />
       </TouchableOpacity>
       <View style={styles.textContainer}>
@@ -40,17 +70,36 @@ const MinuteItem = ({
           </View>
         </TouchableOpacity>
       </View>
-      <View style={styles.rightIcon} onPress={onPress}>
-        <TouchableOpacity style={[styles.summary, {marginRight: 5}]}>
+      <View style={styles.rightIcon}>
+        <TouchableOpacity
+          style={[styles.summary, {marginRight: 5}]}
+          onPress={showDialog}>
           <Icon name="edit" size={30} />
           <Text style={styles.summaryText}>수정</Text>
         </TouchableOpacity>
+        <View>
+          <Dialog.Container visible={visible}>
+            <Dialog.Title>정보 수정</Dialog.Title>
+            <Dialog.Description>변경 사항을 입력해주세요.</Dialog.Description>
+            <Dialog.Input
+              value={itemTitle}
+              onChangeText={setitemTitle}
+              onPressIn={() => console.log(visible)}
+            />
+            <Dialog.Input
+              value={itemDepartment}
+              onChangeText={setitemDepartment}
+            />
+            <Dialog.Button label="취소" onPress={handleCancel} />
+            <Dialog.Button label="확인" onPress={handleOk} />
+          </Dialog.Container>
+        </View>
         <TouchableOpacity style={[styles.summary, {marginRight: 5}]}>
           <Icon name="wysiwyg" size={30} color="#C0C0C0" />
           <Text style={styles.summaryText}>요약</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.summary} onPress={() => onRemove(id)}>
-          <Icon name="delete" size={30} color="red" />
+          <Icon name="delete" size={30} color="#cd5c5c" />
           <Text style={styles.summaryText}>삭제</Text>
         </TouchableOpacity>
       </View>
