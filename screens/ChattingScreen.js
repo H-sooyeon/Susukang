1811fd8382a8049bdc0ac47d90d18ba5bac0284e
H-sooyeon <wndqhr__@naver.com>
2023-05-08 23:34:15 +0900

    feat: header 추가

--- screens/ChattingScreen.js
+++ screens/ChattingScreen.js
@@ -1,17 +1,156 @@
-import React from 'react';
-import {Text, View, StatusBar} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import {Dropdown} from 'react-native-element-dropdown';
+import Dialog from 'react-native-dialog';
 
 const ChattingScreen = ({route, navigation}) => {
+  const [language, setLanguage] = useState(route.params.languageName);
+  const [category, setCategory] = useState(route.params.Category);
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [isFocus, setIsFocus] = useState(false);
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: `${language} ${category}`,
+    });
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={showDialog}>
+          <Text style={styles.languageChange}>변경</Text>
+        </TouchableOpacity>
+      ),
+      headerTitleStyle: {
+        flexShrink: 1,
+        fontSize: 16,
+      },
+    });
+  });
+
+  const showDialog = () => {
+    setVisible(true);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const handleOk = () => {
+    setLanguage(selectedLanguage);
+    setCategory(selectedCategory);
+    // 서버로부터의 번역 언어 재설정
+    setVisible(false);
+  };
+
   return (
-    <View>
+    <View style={styles.Container}>
       <StatusBar backgroundColor="#FFFFFF" barStyle="dark-content" />
-      <Text>Hello! This is Chatting Screen</Text>
-      <Text>selected language: {route.params.languageName}</Text>
-      <Text>selected languageNumber: {route.params.languageNumber}</Text>
-      <Text>selected category: {route.params.categoryName}</Text>
-      <Text>selected categoryNumber: {route.params.categoryNumber}</Text>
+      <View style={{backgroundColor: '#fff', padding: 20, borderRadius: 15}}>
+        <Dialog.Container visible={visible}>
+          <Dialog.Title style={styles.changeTitle}>언어 설정 변경</Dialog.Title>
+          <Dropdown
+            style={[styles.dropdown, isFocus && {borderColor: 'blue'}]}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            inputSearchStyle={styles.inputSearchStyle}
+            iconStyle={styles.iconStyle}
+            data={route.params.Language}
+            search
+            maxHeight={300}
+            labelField="label"
+            valueField="value"
+            placeholder={!isFocus ? 'Select Language' : '...'}
+            searchPlaceholder="Search..."
+            value={language}
+            onFocus={() => setIsFocus(true)}
+            onBlur={() => setIsFocus(false)}
+            onChange={item => {
+              setSelectedLanguage(item.label);
+              setIsFocus(false);
+            }}
+          />
+          <Dropdown
+            style={[styles.dropdown, isFocus && {borderColor: 'blue'}]}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            inputSearchStyle={styles.inputSearchStyle}
+            iconStyle={styles.iconStyle}
+            data={route.params.Category}
+            search
+            maxHeight={300}
+            labelField="label"
+            valueField="value"
+            placeholder={!isFocus ? 'Select Category' : '...'}
+            searchPlaceholder="Search..."
+            value={category}
+            onFocus={() => setIsFocus(true)}
+            onBlur={() => setIsFocus(false)}
+            onChange={item => {
+              setSelectedCategory(item.label);
+              setIsFocus(false);
+            }}
+          />
+          <Dialog.Button label="취소" onPress={handleCancel} />
+          <Dialog.Button label="확인" onPress={handleOk} />
+        </Dialog.Container>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  Container: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  languageChange: {
+    color: '#1976D2',
+    marginLeft: 3,
+  },
+  title: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  titleText: {
+    fontSize: 17,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  placeholderStyle: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    marginBottom: 10,
+  },
+  changeTitle: {
+    marginBottom: 13,
+  },
+});
 
 export default ChattingScreen;
