93ee13297652b8c76d0529b4f723390c9eeeb778
H-sooyeon <wndqhr__@naver.com>
2023-05-12 23:35:06 +0900

    style: 말풍선 사이 margin 간격 추가

--- components/SpeechBubble.js
+++ components/SpeechBubble.js
@@ -22,13 +22,15 @@ const styles = StyleSheet.create({
   containerLeft: {
     alignItems: 'flex-start',
     boxShadow: '2px 2px 3px #d1d1d1',
+    marginBottom: 10,
   },
   containerRight: {
     alignItems: 'flex-end',
     boxShadow: '2px 2px 3px #d1d1d1',
+    marginBottom: 10,
   },
   textRight: {
-    fontSize: 15,
+    fontSize: 18,
     color: 'white',
     backgroundColor: '#148CFF',
     borderRadius: 17,
@@ -36,10 +38,10 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
     paddingLeft: 12,
     paddingRight: 12,
-    marginRight: 20,
+    marginRight: 15,
   },
   textLeft: {
-    fontSize: 15,
+    fontSize: 18,
     color: 'black',
     backgroundColor: '#C8D7FF',
     borderRadius: 17,
@@ -47,7 +49,7 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
     paddingLeft: 12,
     paddingRight: 12,
-    marginLeft: 20,
+    marginLeft: 15,
   },
 });
 
