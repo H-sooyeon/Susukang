7449a7be2bbb5f6b104831b75e94722024fff5ee
H-sooyeon <wndqhr__@naver.com>
2023-05-16 00:22:32 +0900

    feat: Context Container 추가

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {UserContextProvider} from './contexts/UserContext';
 import RootStack from './screens/RootStack';
+import {FileContextProvider} from './contexts/FileContext';
 
 function App() {
   return (
     <UserContextProvider>
-      <NavigationContainer>
-        <RootStack />
-      </NavigationContainer>
+      <FileContextProvider>
+        <NavigationContainer>
+          <RootStack />
+        </NavigationContainer>
+      </FileContextProvider>
     </UserContextProvider>
   );
 }
