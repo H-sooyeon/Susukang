0ea00e563a25b24e7d47fa68ec69568ad57ba3a0
H-sooyeon <wndqhr__@naver.com>
2023-05-16 00:25:18 +0900

    feat: 수정 후 저장 기능 추가

--- screens/OpenMinute.js
+++ screens/OpenMinute.js
@@ -1,28 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import {View, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import FileContext from '../contexts/FileContext';
 
 const OpenMinute = ({navigation, route}) => {
-  const [content, setContent] = useState(route.params.content);
-  const [datas, setDatas] = useState(route.params.datas);
+  const {id, title, department, content, date} = route.params.file;
+
+  const [filecontent, setFileContent] = useState(content);
+  const {onModify} = useContext(FileContext);
 
   // 서버와 연결해서 내용 수정, 저장하는 기능 추가(아래 코드 수정)
-  const onToggle = (id, content, title, department) => {
-    setDatas(datas =>
-      datas.map(data =>
-        data.id === id
-          ? {...data, content: content, title: title, department: department}
-          : data,
-      ),
-    );
+
+  const getDate = today => {
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+
+    const date = `${year}.${month}.${day}`;
+
+    return date;
+  };
+
+  const onContentSave = () => {
+    onModify({
+      id: id,
+      title: title,
+      department: department,
+      content: filecontent,
+      date: getDate(new Date()),
+    });
   };
 
   useEffect(() => {
-    navigation.setOptions({title: route.params.title});
+    navigation.setOptions({title: title});
     navigation.setOptions({
       headerRight: () => (
-        <TouchableOpacity onPress={() => onToggle(route.params.id)}>
-          <Icon name="save" size={20} color="black" />
+        <TouchableOpacity onPress={onContentSave}>
+          <Icon name="check" size={20} color="black" />
         </TouchableOpacity>
       ),
     });
@@ -33,8 +47,8 @@ const OpenMinute = ({navigation, route}) => {
       <TextInput
         style={styles.content}
         multiline={true}
-        onChangeText={setContent}
-        value={content}
+        onChangeText={setFileContent}
+        value={filecontent}
       />
     </View>
   );
