bb78d4de20d3ab5ab46dcf466da4403fddc32065
H-sooyeon <wndqhr__@naver.com>
2023-05-16 22:33:17 +0900

    feat: 스케쥴, 설정 context 추가

--- App.js
+++ App.js
@@ -3,15 +3,24 @@ import {NavigationContainer} from '@react-navigation/native';
 import {UserContextProvider} from './contexts/UserContext';
 import RootStack from './screens/RootStack';
 import {FileContextProvider} from './contexts/FileContext';
+import {SettingContextProvider} from './contexts/SettingContext';
+import {SearchContextProvider} from './contexts/SearchContext';
+import {ScheduleContextProvider} from './contexts/ScheduleContext';
 
 function App() {
   return (
     <UserContextProvider>
-      <FileContextProvider>
-        <NavigationContainer>
-          <RootStack />
-        </NavigationContainer>
-      </FileContextProvider>
+      <SearchContextProvider>
+        <ScheduleContextProvider>
+          <FileContextProvider>
+            <SettingContextProvider>
+              <NavigationContainer>
+                <RootStack />
+              </NavigationContainer>
+            </SettingContextProvider>
+          </FileContextProvider>
+        </ScheduleContextProvider>
+      </SearchContextProvider>
     </UserContextProvider>
   );
 }
