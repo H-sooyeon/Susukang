ddc9da3c0d4e7f84071ff04ef96ed39cc51f18c2
H-sooyeon <wndqhr__@naver.com>
2023-06-07 15:46:00 +0900

    feat: 경고 메시지 사용자 화면 숨김

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {LogBox} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {UserContextProvider} from './contexts/UserContext';
 import RootStack from './screens/RootStack';
@@ -8,6 +9,13 @@ import {SearchContextProvider} from './contexts/SearchContext';
 import {ScheduleContextProvider} from './contexts/ScheduleContext';
 import {KakaoContextProvider} from './contexts/KakaoContext';
 import {STTContextProvider} from './contexts/STTContext';
+
+LogBox.ignoreLogs([
+  '`new NativeEventEmitter()` was called with a non-null argument without the required `addListener` method.',
+]);
+LogBox.ignoreLogs([
+  '`new NativeEventEmitter()` was called with a non-null argument without the required `removeListeners` method.',
+]);
 
 function App() {
   return (
