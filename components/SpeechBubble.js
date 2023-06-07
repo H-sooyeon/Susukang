79aa525e16082f830c492936c06d9e3f42b2a7c9
H-sooyeon <wndqhr__@naver.com>
2023-06-07 15:36:28 +0900

    style: 말풍선 디자인 수정

--- components/SpeechBubble.js
+++ components/SpeechBubble.js
@@ -1,9 +1,47 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Tts from 'react-native-tts';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const SpeechBubble = ({text, direction, languageCode}) => {
-  const rightBotomText = 'hello';
-  const leftBottomText = '안녕하세요';
+const SpeechBubble = ({
+  text,
+  direction,
+  languageCode,
+  translateText,
+  stopRecognizing,
+  startRecognizing,
+  isRecording,
+}) => {
+  useEffect(() => {
+    // 컴포넌트가 언마운트될 때 음성 출력을 중지합니다.
+    return () => {
+      Tts.stop();
+    };
+  }, []);
+
+  const onTTS = async () => {
+    try {
+      Tts.setDefaultVoice(languageCode + '-default');
+      Tts.setDefaultLanguage(languageCode);
+    } catch (error) {
+      console.error('Error occurred during TTS: ', error);
+    }
+
+    if (isRecording) {
+      await stopRecognizing();
+
+      if (translateText) {
+        Tts.speak(translateText);
+      }
+
+      startRecognizing();
+    } else {
+      if (translateText) {
+        Tts.speak(translateText);
+      }
+    }
+  };
+
   return (
     <View
       style={
@@ -15,19 +53,20 @@ const SpeechBubble = ({text, direction, languageCode}) => {
           direction === 'left' ? styles.textLeft : styles.textRight,
         ]}>
         {text}
-        {'\n'}
-        <View style={styles.bottomContainer}>
-          <Text
-            style={[
-              direction === 'right'
-                ? styles.bottomTextRight
-                : styles.bottomTextLeft,
-            ]}>
-            <Text>
-              {direction === 'left' ? leftBottomText : rightBotomText}
-            </Text>
-          </Text>
-        </View>
+        {'\n\n'}
+        <Text
+          style={[
+            direction === 'right'
+              ? styles.bottomTextRight
+              : styles.bottomTextLeft,
+          ]}>
+          {translateText}
+        </Text>
+        <TouchableOpacity style={styles.volume} onPress={onTTS}>
+          <View>
+            <Icon name="volume-up" size={17} color="#1976D2" />
+          </View>
+        </TouchableOpacity>
       </Text>
     </View>
   );
@@ -45,6 +84,10 @@ const styles = StyleSheet.create({
     boxShadow: '2px 2px 3px #d1d1d1',
     marginBottom: 10,
     marginLeft: 100,
+  },
+  volume: {
+    paddingLeft: 10,
+    alignSelf: 'flex-end',
   },
   textRight: {
     fontSize: 18,
@@ -72,8 +115,10 @@ const styles = StyleSheet.create({
     marginLeft: 10,
   },
   bottomTextRight: {
-    color: '#28288C',
+    color: '#5a5a5a',
+    fontWeight: 100,
     fontSize: 18,
+    flexWrap: 'wrap',
   },
   bottomTextLeft: {
     color: '#1976D2',
